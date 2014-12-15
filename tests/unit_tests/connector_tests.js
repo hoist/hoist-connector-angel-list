10d1e91aec4c9288155edd744cd56c8a9fee8990
Amelia Laundy <amelialaundy@gmail.com>
2014-12-16 11:32:48 +1300

    testing get #request with unauthenticated connector type

--- tests/unit_tests/connector_tests.js
+++ tests/unit_tests/connector_tests.js
@@ -18,12 +18,8 @@ describe('AngelListConnector', function () {
     connector.authSettings = {
       authProxy: {
         token: {
-          access_token: '<access_token>',
-          refresh_token: '<refresh_token>',
-          expires: Date.now() + 10 * 60 * 1000,
-          expires_in: '7200'
-        },
-        domainPrefix: 'test'
+          access_token: '<access_token>'
+        }
       },
       get: function (name) {
         return this.authProxy[name];
@@ -126,24 +122,229 @@ describe('AngelListConnector', function () {
       });
     });
   });
+  describe('#delete', function () {
+    describe('with no data', function () {
+      var response = {};
+      var result;
+      before(function () {
+        sinon.stub(connector, 'request').returns(BBPromise.resolve(response));
+        result = connector.delete('/comments/1');
+      });
+      after(function () {
+        connector.request.restore();
+      });
+      it('calls #request', function () {
+        expect(connector.request)
+          .to.have.been.calledWith('DELETE', '/comments/1');
+      });
+    });
+    describe('with data', function () {
+      var response = {};
+      var result;
+      var data = {
+        authenticatedRequest: true
+      };
+      before(function () {
+        sinon.stub(connector, 'request').returns(BBPromise.resolve(response));
+        result = connector.delete('/comments/1', data);
+      });
+      after(function () {
+        connector.request.restore();
+      });
+      it('calls #request', function () {
+        expect(connector.request)
+          .to.have.been.calledWith('DELETE', '/comments/1', null, data);
+      });
+    });
+  });
 
   describe('#request', function () {
     describe('GET', function () {
       describe('with authenticated connector type', function () {
-        
         before(function () {
           connector.settings.authType = 'authenticated';
-         
         });
-        describe('with authenticatedRequest false', function () {
-          it('rejects', function () {
-            expect(function () {
-              connector.request('GET', '/product', null, null);
-            }).to.throw(errors.connector.request.InvalidError);
+        describe('with no queryParams', function () {
+          var response = {
+            body: {
+              body: "some body"
+            },
+            statusCode: 200
+          };
+          var options = {
+            method: 'GET',
+            uri: 'https://api.angel.co/1/product',
+            headers: {
+              Authorization: 'Bearer <access_token>'
+            },
+            json: true,
+            resolveWithFullResponse: true
+          };
+          var result;
+          before(function () {
+            sinon.stub(connector, 'requestPromiseHelper').returns(BBPromise.resolve(response));
+            result = connector.request('GET', '/product', null);
+          });
+          after(function () {
+            connector.requestPromiseHelper.restore();
+          });
+          it('calls requestPromiseHelper', function () {
+            expect(connector.requestPromiseHelper)
+              .to.have.been.calledWith(options);
+          });
+          it('returns the response body', function () {
+            expect(result)
+              .to.become(response.body);
           });
         });
-        describe('with authenticatedRequest true', function () {
+        describe('with queryParams object', function () {
+          var response = {
+            body: {
+              body: "some body"
+            },
+            statusCode: 200
+          };
           
+          var queryParams = {
+            query: 'query'
+          };
+          var options = {
+            method: 'GET',
+            uri: 'https://api.angel.co/1/product?query=query',
+            headers: {
+              Authorization: 'Bearer <access_token>'
+            },
+            json: true,
+            resolveWithFullResponse: true
+          };
+          var result;
+          before(function () {
+            sinon.stub(connector, 'requestPromiseHelper').returns(BBPromise.resolve(response));
+            result = connector.request('GET', '/product', queryParams);
+          });
+          after(function () {
+            connector.requestPromiseHelper.restore();
+          });
+          it('calls requestPromiseHelper', function () {
+            expect(connector.requestPromiseHelper)
+              .to.have.been.calledWith(options);
+          });
+          it('returns the response body', function () {
+            expect(result)
+              .to.become(response.body);
+          });
+        });
+        describe('with queryParams in path', function () {
+          var response = {
+            body: {
+              body: "some body"
+            },
+            statusCode: 200
+          };
+          var options = {
+            method: 'GET',
+            uri: 'https://api.angel.co/1/product?query=query',
+            headers: {
+              Authorization: 'Bearer <access_token>'
+            },
+            json: true,
+            resolveWithFullResponse: true
+          };
+          var result;
+          before(function () {
+            sinon.stub(connector, 'requestPromiseHelper').returns(BBPromise.resolve(response));
+            result = connector.request('GET', '/product?query=query', null);
+          });
+          after(function () {
+            connector.requestPromiseHelper.restore();
+          });
+          it('calls requestPromiseHelper', function () {
+            expect(connector.requestPromiseHelper)
+              .to.have.been.calledWith(options);
+          });
+        });
+        describe('with queryParams in path and object', function () {
+          var response = {
+            body: {
+              body: "some body"
+            },
+            statusCode: 200
+          };
+          var queryParams = {
+            query: 'query'
+          };
+          var options = {
+            method: 'GET',
+            uri: 'https://api.angel.co/1/product?querypath=querypath&query=query',
+            headers: {
+              Authorization: 'Bearer <access_token>'
+            },
+            json: true,
+            resolveWithFullResponse: true
+          };
+          var result;
+          before(function () {
+            sinon.stub(connector, 'requestPromiseHelper').returns(BBPromise.resolve(response));
+            result = connector.request('GET', '/product?querypath=querypath', queryParams);
+          });
+          after(function () {
+            connector.requestPromiseHelper.restore();
+          });
+          it('calls requestPromiseHelper', function () {
+            expect(connector.requestPromiseHelper)
+              .to.have.been.calledWith(options);
+          });
+        });
+        describe('with duplicate queryParams in path and object', function () {
+          var response = {
+            body: {
+              body: "some body"
+            },
+            statusCode: 200
+          };
+          var queryParams = {
+            query: 'query'
+          };
+          var options = {
+            method: 'GET',
+            uri: 'https://api.angel.co/1/product?query=query',
+            headers: {
+              Authorization: 'Bearer <access_token>'
+            },
+            json: true,
+            resolveWithFullResponse: true
+          };
+          var result;
+          before(function () {
+            sinon.stub(connector, 'requestPromiseHelper').returns(BBPromise.resolve(response));
+            result = connector.request('GET', '/product?query=queryfalse', queryParams);
+          });
+          after(function () {
+            connector.requestPromiseHelper.restore();
+          });
+          it('calls requestPromiseHelper correctly', function () {
+            expect(connector.requestPromiseHelper)
+              .to.have.been.calledWith(options);
+          });
+        });
+      });
+      describe('with unauthenticated connector type', function () {
+        
+        before(function () {
+          connector.settings.authType = 'unauthenticated';
+         
+        });
+        describe('with authenticatedRequest true', function () {
+          var data = {
+            authenticatedRequest: true
+          };
+          it('rejects', function () {
+            expect(function () {
+              connector.request('GET', '/product', null, data);
+            }).to.throw(errors.connector.request.UnauthorizedError);
+          });
+        });
+        describe('with authenticatedRequest false', function () {
           describe('with no queryParams', function () {
             var response = {
               body: {
@@ -152,22 +353,18 @@ describe('AngelListConnector', function () {
               statusCode: 200
             };
             var data = {
-              authenticatedRequest: true
+              authenticatedRequest: false
             };
             var options = {
               method: 'GET',
               uri: 'https://api.angel.co/1/product',
-              headers: {
-                Authorization: 'Bearer <access_token>'
-              },
               json: true,
               resolveWithFullResponse: true
             };
             var result;
             before(function () {
               sinon.stub(connector, 'requestPromiseHelper').returns(BBPromise.resolve(response));
-              console.log('data: ', data)
-              result = connector.request('GET', '/product', null, data);
+              result = connector.request('GET', '/product', null);
             });
             after(function () {
               connector.requestPromiseHelper.restore();
@@ -188,25 +385,19 @@ describe('AngelListConnector', function () {
               },
               statusCode: 200
             };
-            var data = {
-              authenticatedRequest: true
-            };
             var queryParams = {
               query: 'query'
             };
             var options = {
               method: 'GET',
               uri: 'https://api.angel.co/1/product?query=query',
-              headers: {
-                Authorization: 'Bearer <access_token>'
-              },
               json: true,
               resolveWithFullResponse: true
             };
             var result;
             before(function () {
               sinon.stub(connector, 'requestPromiseHelper').returns(BBPromise.resolve(response));
-              result = connector.request('GET', '/product', queryParams, data);
+              result = connector.request('GET', '/product', queryParams);
             });
             after(function () {
               connector.requestPromiseHelper.restore();
@@ -230,19 +421,13 @@ describe('AngelListConnector', function () {
             var options = {
               method: 'GET',
               uri: 'https://api.angel.co/1/product?query=query',
-              headers: {
-                Authorization: 'Bearer <access_token>'
-              },
               json: true,
               resolveWithFullResponse: true
-            };
-            var data = {
-              authenticatedRequest: true
             };
             var result;
             before(function () {
               sinon.stub(connector, 'requestPromiseHelper').returns(BBPromise.resolve(response));
-              result = connector.request('GET', '/product?query=query', null, data);
+              result = connector.request('GET', '/product?query=query', null);
             });
             after(function () {
               connector.requestPromiseHelper.restore();
@@ -265,20 +450,13 @@ describe('AngelListConnector', function () {
             var options = {
               method: 'GET',
               uri: 'https://api.angel.co/1/product?querypath=querypath&query=query',
-              headers: {
-                Authorization: 'Bearer <access_token>'
-              },
               json: true,
               resolveWithFullResponse: true
-            };
-            var data = {
-              authenticatedRequest: true
             };
             var result;
             before(function () {
               sinon.stub(connector, 'requestPromiseHelper').returns(BBPromise.resolve(response));
-              console.log('data: ', data)
-              result = connector.request('GET', '/product?querypath=querypath', queryParams, data);
+              result = connector.request('GET', '/product?querypath=querypath', queryParams);
             });
             after(function () {
               connector.requestPromiseHelper.restore();
@@ -288,7 +466,7 @@ describe('AngelListConnector', function () {
                 .to.have.been.calledWith(options);
             });
           });
-          describe.skip('with duplicate queryParams in path and object', function () {
+          describe('with duplicate queryParams in path and object', function () {
             var response = {
               body: {
                 body: "some body"
@@ -301,32 +479,20 @@ describe('AngelListConnector', function () {
             var options = {
               method: 'GET',
               uri: 'https://api.angel.co/1/product?query=query',
-              headers: {
-                Authorization: 'Bearer <access_token>'
-              },
               json: true,
               resolveWithFullResponse: true
-            };
-            var data = {
-              authenticatedRequest: true
             };
             var result;
             before(function () {
               sinon.stub(connector, 'requestPromiseHelper').returns(BBPromise.resolve(response));
-              sinon.stub(connector, 'refreshToken').returns(BBPromise.resolve());
               result = connector.request('GET', '/product?query=queryfalse', queryParams);
             });
             after(function () {
               connector.requestPromiseHelper.restore();
-              connector.refreshToken.restore();
             });
             it('calls requestPromiseHelper correctly', function () {
               expect(connector.requestPromiseHelper)
                 .to.have.been.calledWith(options);
-            });
-            it('calls refreshToken', function () {
-              expect(connector.refreshToken)
-                .to.have.been.called;
             });
           });
         });
